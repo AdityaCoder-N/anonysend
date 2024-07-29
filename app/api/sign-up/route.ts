4f3bdba2ee179cf54b97898e62501b9da98ad53d
thelostwarrior69 <91337266+AdityaCoder-N@users.noreply.github.com>
2024-07-29 15:37:05 +0530

    removed and cleaned resend code

--- app/api/sign-up/route.ts
+++ app/api/sign-up/route.ts
@@ -1,8 +1,6 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import bcrypt from 'bcryptjs'
-
-import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 import { sendEmail } from "@/lib/mail.utils";
 
 export async function POST(request:Request){
@@ -68,8 +66,6 @@ export async function POST(request:Request){
         }
 
         //send verification email
-        // const emailResponse = await sendVerificationEmail(email,username,verifyCode);
-
         const emailResponse = await sendEmail({email,username,verifyCode});
 
         if(!emailResponse.success){
